b0a7fa24aab7037a59cb9bce753220ec40a28437
ShaniCattan <323shani@gmail.com>
2020-06-21 22:17:34 +0300

    remove landscape, added progress seekbar and "no_server_updated_button" buttons.

--- app/src/main/java/com/example/flightmobileapp/Joystick.kt
+++ app/src/main/java/com/example/flightmobileapp/Joystick.kt
@@ -1,20 +1,58 @@
 package com.example.flightmobileapp
 
+import android.os.Build
 import android.os.Bundle
 import android.widget.SeekBar
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import io.github.controlwear.virtual.joystick.android.JoystickView
 import kotlinx.android.synthetic.main.joystick.*
 
-class Joystick : AppCompatActivity(){
+class Joystick : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.joystick)
-        val joystick = findViewById<JoystickView>(R.id.joystickView)
-        joystick.setOnMoveListener { angle, strength ->
-
-        }
+        setSeekBarProgress()
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun setSeekBarProgress(){
+        val throttleSB = findViewById<SeekBar>(R.id.throttleSeekBar)
+        throttleSB.max = 10
+        val rudderSB = findViewById<SeekBar>(R.id.rudderSeekBar)
+        rudderSB.max = 10
+        rudderSB.min = -10
+        val throttleText = findViewById<TextView>(R.id.thrText)
+        val rudderText = findViewById<TextView>(R.id.rudText)
+        throttleSB?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                val num:Double = (progress)*0.1
+                throttleText.text =   String.format("%.1f", num)
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // Write code to perform some action when touch is started.
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // Write code to perform some action when touch is stopped.
+            }
+        })
+
+        rudderSB?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                val num:Double = (progress)*0.1
+                rudderText.text =   String.format("%.1f", num)
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // Write code to perform some action when touch is started.
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // Write code to perform some action when touch is stopped.
+            }
+        })
+    }
 }
--- app/src/main/java/com/example/flightmobileapp/MainActivity.kt
+++ app/src/main/java/com/example/flightmobileapp/MainActivity.kt
@@ -14,17 +14,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.connect_button)
         }
-    //connect to server, move to the next activity
-    fun doneCLicked (view: View){
-  //      val url = view.findViewById<EditText>(R.id.url).toString()
-   //     val imageView = view.findViewById<ImageView>(R.id.server_screenshot)
-  //      Picasso.get().load(url).placeholder(R.drawable.error).error(R.drawable.error).into(imageView)
-        val intent = Intent(this,Joystick::class.java)
+    fun connectClick(view:View){
+        val intent = Intent(this, Joystick::class.java)
         startActivity(intent)
     }
-}
+
+    }
+
 
 
 
